062f6433181ffd24ad517d013485db9f01f48cd4
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-08-10 12:03:46 -0300

    Added database configuration by env

--- lib/blog.ex
+++ lib/blog.ex
@@ -12,11 +12,13 @@ defmodule Blog do
 end
 
 defmodule Repo do
-  use Ecto.Repo, adapter: Ecto.Adapters.Postgres
+  use Ecto.Repo, adapter: Ecto.Adapters.Postgres, env: Mix.env
 
-  def conf do
-    parse_url "ecto://postgres:postgres@localhost/ecto_simple"
-  end
+  def conf(env), do: parse_url url(env)
+
+  defp url(:dev),  do: "ecto://postgres:postgres@localhost/blog_dev"
+  defp url(:test), do: "ecto://postgres:postgres@localhost/blog_test?size=1"
+  defp url(:prod), do: "ecto://postgres:postgres@localhost/blog_prod"
 
   def priv do
     app_dir(:blog, "priv/repo")
